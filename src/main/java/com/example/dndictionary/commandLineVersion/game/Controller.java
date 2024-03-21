7a60705969bc44d1b501d55ec8299ea4f76a84db
mai duc <maiduc0803@gmail.com>
2024-03-21 14:25:09 +0700

    update game controller

---
+++ src/main/java/com/example/dndictionary/commandLineVersion/game/Controller.java
@@ -0,0 +1,16 @@
+package com.example.dndictionary.commandLineVersion.game;
+
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public abstract class Controller {
+    protected Parent root;
+    protected Stage stage;
+    protected Scene scene;
+
+    @FXML
+    protected AnchorPane rootAnchor;
+}
